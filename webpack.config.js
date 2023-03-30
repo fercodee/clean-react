a43cc60073dc1460f0f94a2e54aa942644eb0e0f
fercodee <devbyfull@gmail.com>
2023-03-30 06:49:29 -0300

    chore: config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -7,5 +8,23 @@ module.exports = {
         path: path.join(__dirname, 'public/js'),
         publicPath: '/public/js',
         fileName: 'bundle.js'
-    }
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+        alias: {
+            '@': path.join(__dirname, 'src')
+        }
+    },
+    devserver: {
+        contentBase: './public',
+        writeToDisk: true,
+        hsitoryApiFallback: true 
+    },
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    },
+    plugins: [ 
+        new CleanWebpackPlugin()
+    ]
 }
