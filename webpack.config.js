d06bafa045ded5e392bd6d1b67d79f2c47b8aaf1
fercodee <devbyfull@gmail.com>
2023-03-30 07:15:47 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,55 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/main/index.tsx',
-    output: {
-        path: path.join(__dirname, 'public/js'),
-        publicPath: '/public/js',
-        fileName: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-        alias: {
-            '@': path.join(__dirname, 'src')
-        }
-    },
-    devserver: {
-        contentBase: './public',
-        writeToDisk: true,
-        hsitoryApiFallback: true 
-    },
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDOM'
-    },
-    plugins: [ 
-        new CleanWebpackPlugin()
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    fileName: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
     ]
+  },
+  devserver: {
+    contentBase: './public',
+    writeToDisk: true,
+    hsitoryApiFallback: true
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
